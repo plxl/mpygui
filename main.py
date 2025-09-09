721a819a67aea123c9142e422f1c49b02c896335
plxl <210124508+plxl@users.noreply.github.com>
2025-09-09 13:07:21 +0800

    ADD basic Drag & Drop support to the sidebar

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import customtkinter as ctk
 from app.theme_manager import init_theme
 from app.logger import log
+from tkinterdnd2 import DND_FILES, TkinterDnD
 
 PD = 10 # global padding
 CR = 20 # global corner radius
 
-class App(ctk.CTk):
+class App(ctk.CTk, TkinterDnD.DnDWrapper):
     def __init__(self):
         super().__init__()
+        self.TkdndVersion = TkinterDnD._require(self)  
 
         self.title("mPyGUI: The ffmpeg GUI (Python Edition)")
         self.set_center()
@@ -30,6 +32,13 @@ class App(ctk.CTk):
     def create_sidebar(self):
         self.sidebar = ctk.CTkFrame(self, corner_radius=CR)
         self.sidebar.grid(row=0, rowspan=1, column=0, padx=PD, pady=PD, sticky="ns")
+        
+        self.sidebar.drop_target_register(DND_FILES)
+        self.sidebar.dnd_bind("<<Drop>>", self.sidebar_on_drop)
+        
+    def sidebar_on_drop(self, event):
+        print(event.data)
+        pass
 
 def main():
     init_theme()
