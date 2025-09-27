29b9639d9544f1e1ae1072bc7888cd987f25b71c
plxl <210124508+plxl@users.noreply.github.com>
2025-09-27 16:51:23 +0800

    UPDATE CTkCustomListbox to only show scrollbar when its needed
    Why is this not default behaviour? Without forking the original, I have to do this kinda hacky colour changing which is fine and works with themes but still...

--- app/ctk_custom_listbox.py
+++ app/ctk_custom_listbox.py
@@ -1,3 +1,4 @@
+import customtkinter as ctk
 from CTkListbox import CTkListbox
 
 """
@@ -6,6 +7,11 @@ in order to select additional items (preferred over click toggle)
 """
 
 class CTkCustomListbox(CTkListbox):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._orig_fg = self._scrollbar.cget("fg_color")
+        self.after(50, self._refresh_scrollbar)
+    
     def insert(self, index, option, update=True, **args):
         """Insert item with modified click behaviour"""
         button = super().insert(index, option, update=update, **args)
@@ -39,3 +45,22 @@ class CTkCustomListbox(CTkListbox):
             self.deselect(index)
         else:
             self.select(index)
+
+    def _refresh_scrollbar(self):
+        self.update_idletasks()
+        low, high = self._parent_canvas.yview()
+        # (0.0, 1.0) if fits
+        
+        # if scrollbar is not needed
+        if float(low) <= 0.0 and float(high) >= 1.0:
+            self._scrollbar.configure(
+                button_color=ctk.ThemeManager.theme["CTkFrame"]["fg_color"],
+                button_hover_color=ctk.ThemeManager.theme["CTkFrame"]["fg_color"],
+            )
+        else:
+            self._scrollbar.configure(
+                button_color=ctk.ThemeManager.theme["CTkScrollbar"]["button_color"],
+                button_hover_color=ctk.ThemeManager.theme["CTkScrollbar"]["button_hover_color"],
+            )
+            
+        self.after(200, self._refresh_scrollbar)
