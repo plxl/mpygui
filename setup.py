b24ef84ed9d683c3d82a54cb189b42c2c2854c1b
plxl <210124508+plxl@users.noreply.github.com>
2025-09-28 21:07:37 +0800

    ADD toml and setup.py installing and building configs

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+APP = ["src/mpygui/launch.py"]
+OPTIONS = {
+    "packages": ["mpygui", "common"],
+}
+
+setup(
+    app=APP,
+    options={"py2app": OPTIONS},
+    setup_requires=["py2app"],
+)
