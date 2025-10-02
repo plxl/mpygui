d802e661e4b4d9a972185b0ecf40103f816a1b2c
plxl <210124508+plxl@users.noreply.github.com>
2025-10-02 20:41:17 +0800

    ADD comments for clarity in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
-# setup.py
+# this is for building on macOS with py2app only
+
+# for Windows/Linux, use:
+# pyinstaller --onefile --windowed -n mPyGUI src/mpygui/launch.py
+
+# on macOS:
+# python setup.py py2app
+
 from setuptools import setup
 
 APP = ["src/mpygui/launch.py"]
